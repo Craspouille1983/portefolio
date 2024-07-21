d0bfb9ebdde47ca2d78581baeafa112de7e850aa
Craspouille1983 <frederic.sejournant@outlook.fr>
2024-07-21 16:56:30 +0200

    Avancement sur les compétences

--- portefolio/portefolio/settings.py
+++ portefolio/portefolio/settings.py
@@ -78,14 +78,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'users': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'folio',
-        'USER': 'root',
-        'PASSWORD': 'Q12345-è_',
-        'HOST': 'localhost',
-        'PORT': '8000',
     }
 }
 
