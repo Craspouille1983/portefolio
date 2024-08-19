9f47d8a22ec3f7bd849986b93acc7c7ff7f76d36
Craspouille1983 <frederic.sejournant@outlook.fr>
2024-08-19 08:45:27 +0200

    Correction oubli dans le settings

--- portefolio/portefolio/settings.py
+++ portefolio/portefolio/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-# ALLOWED_HOSTS = ['portfolio-fs.codephenix.fr']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['portfolio-fs.codephenix.fr']
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
