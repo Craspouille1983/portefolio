eb7f0524f0d13b77204f9b6e631f963ef22b53e3
Craspouille <craspouille@craspouille>
2024-09-26 23:49:23 +0200

    ...

--- portefolio/portefolio/settings.py
+++ portefolio/portefolio/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = 'True'
 
-ALLOWED_HOSTS = ['portfolio-fs.codephenix.fr']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['portfolio-fs.codephenix.fr']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
